29b6d20540bf0d50899be221e3a841d68651aa31
Kaushal Powar <90775147+kaushalpowar@users.noreply.github.com>
2023-04-19 19:06:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,2 @@
 import streamlit as st
-from streamlit.components.v1 import html
-from langchain.document_loaders import UnstructuredPDFLoader
-from langchain.indexes import VectorstoreIndexCreator
+st.title('hey')
