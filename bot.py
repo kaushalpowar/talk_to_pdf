e92a6a4f07189955ef74e5b904bbaa8b992e6265
Kaushal Powar <90775147+kaushalpowar@users.noreply.github.com>
2023-04-19 19:39:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,2 +1,15 @@
 import streamlit as st
-st.title('hey')
+st.title('HI')
+import os
+os.environ["OPENAI_API_KEY"] = "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"
+from langchain.document_loaders import UnstructuredPDFLoader
+from langchain.indexes import VectorstoreIndexCreator
+st.title("hi")
+pdf_folder_path="/Users/kaushal/talk_to_pdf/Mitch Horowitz, Neville Goddard - Infinite Potential_ The Greatest Works of Neville Goddard (22 Oct 2019, St. Martin's Publishing Group_St. Martin’s Essentials) - libgen.li.pdf"
+# location of the pdf file/files.
+loaders = [UnstructuredPDFLoader(os.path.join(pdf_folder_path, fn)for fn in os.listdir(pdf_folder_path))]
+
+index = VectorstoreIndexCreator().from_loaders(loaders)
+
+
+index.query('What is god?')
